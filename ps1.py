8f1896d9908d78811f63f37c9727e4ecdc3ef786
2016JTM2940 <kusunurimounika@gmail.com>
2016-09-12 15:48:50 +0530

    updated code again

--- ps1.py
+++ ps1.py
@@ -9,16 +9,25 @@ from collections import Counter
 #intialise the variables and array
 j=0
 count=0
+k=0
 a={}
 
 #store the command line arguments in list
 for i in range(len(sys.argv)-1):
 	a[j]=sys.argv[i+1]
-	print (a[j])
+#	print (a[j])
 	j=j+1
 
 #print (a.count(1))
 #print (Counter(a))
 	
-for c in a:
-	print(c)
+for i in range(len(sys.argv)-1):
+	for j in range(len(sys.argv)-1):
+		if a[j]==a[i]:
+			count=count+1
+	print(count,a[i])
+	if k < count:
+		k = count
+#		print(k,a[i])
+	count = 0
+#	print(k,a[i])
