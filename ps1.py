c38194dd7b0e56d3b17c9ffa4df0524f3010e9ae
2016JTM2940 <kusunurimounika@gmail.com>
2016-09-12 15:02:40 +0530

    updated code

--- ps1.py
+++ ps1.py
@@ -1,3 +1,24 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+#import sys module
+import sys
+from collections import Counter
+
+#intialise the variables and array
+j=0
+count=0
+a={}
+
+#store the command line arguments in list
+for i in range(len(sys.argv)-1):
+	a[j]=sys.argv[i+1]
+	print (a[j])
+	j=j+1
+
+#print (a.count(1))
+#print (Counter(a))
+	
+for c in a:
+	print(c)
